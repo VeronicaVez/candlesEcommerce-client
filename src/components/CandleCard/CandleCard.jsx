71b897eeaa973d2bb98dd901a2741f11dfbb0d73
Veronica Vezzoli <veronicavezzoli94@gmail.com>
2024-07-01 14:13:58 +0200

    change style + added candles

--- src/components/CandleCard/CandleCard.jsx
+++ src/components/CandleCard/CandleCard.jsx
@@ -1,6 +1,7 @@
 import Button from "react-bootstrap/Button"
-import { Card, CloseButton } from "react-bootstrap"
+import { Card, CloseButton, Container } from "react-bootstrap"
 import { Link } from "react-router-dom"
+import "./CandleCard.css"
 
 function CandleCard({
   _id,
@@ -12,25 +13,25 @@ function CandleCard({
   deleteCandle,
 }) {
   return (
-    <>
-      <Card>
+    <Container>
+      <Card className="candle-card">
         <Card.Body>
           {/*}   <CloseButton
             className="btn-close"
             onClick={() => deleteCandle(_id)}
           /> */}
           <Link to={`/candles/${_id}`} className="link-class">
-            <Card.Img variant="top" src={image} />
-            <Card.Title>
-              {name} - {aroma}
-            </Card.Title>
-            <Card.Subtitle>{price}</Card.Subtitle>
-            <Card.Text>{description}</Card.Text>
+            <Card.Img variant="top" src={image} className="photo-class" />
+            <Card.Title className="title-class">{name}</Card.Title>
+            <Card.Subtitle className="subtitle-class">
+              €{price} - {aroma}
+            </Card.Subtitle>
+            <Card.Text className="text-class">{description}</Card.Text>
           </Link>
-          <Button variant="primary">Add to cart</Button>
+          <Button className="button-class">Add to cart</Button>
         </Card.Body>
       </Card>
-    </>
+    </Container>
   )
 }
 
--- src/pages/CandlesPage/CandlesPage.jsx
+++ src/pages/CandlesPage/CandlesPage.jsx
@@ -3,20 +3,23 @@ import { Button } from "react-bootstrap"
 import { useContext } from "react"
 import { AuthContext } from "../../context/auth.context"
 import { Link } from "react-router-dom"
+import "./CandlesPage.css"
 
 const CandlesPage = () => {
   const { isLoggedIn, user } = useContext(AuthContext)
 
   return (
-    <>
+    <div className="candles-page">
       <h1> Our candles</h1>
       <CandleList />
-      {isLoggedIn && user.role === "admin" ? (
-        <Button as={Link} to="/candles/new-candle">
-          Add New Candle
-        </Button>
-      ) : null}
-    </>
+      <div className="newcandle-button">
+        {isLoggedIn && user.role === "admin" ? (
+          <Button as={Link} to="/candles/new-candle" className="button-class">
+            Add New Candle
+          </Button>
+        ) : null}
+      </div>
+    </div>
   )
 }
 
